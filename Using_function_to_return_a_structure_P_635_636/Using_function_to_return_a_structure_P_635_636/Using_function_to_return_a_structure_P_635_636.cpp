a8a72f2c6eb87294d7cd5e8dee2f7159f8296e63
rhoona <pdavidson@student.rccd.edu>
2019-07-28 11:59:00 -0700

    Adding.

--- Using_function_to_return_a_structure_P_635_636/Using_function_to_return_a_structure_P_635_636/Using_function_to_return_a_structure_P_635_636.cpp
+++ Using_function_to_return_a_structure_P_635_636/Using_function_to_return_a_structure_P_635_636/Using_function_to_return_a_structure_P_635_636.cpp
@@ -1,20 +1,42 @@
-// Using_function_to_return_a_structure_P_635_636.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
+#include <iomanip>
+#include <cmath>  //for pow function.
 #include <iostream>
+using namespace std;
 
-int main()
+const double PI = 3.14159;
+
+struct Circle
 {
-    std::cout << "Hello World!\n";
+	double radius;
+	double diamter;
+	double area;
+};
+
+Circle getInfo();
+
+int main() {
+
+	Circle c;
+	c = getInfo();
+	c.area = PI * pow(c.radius, 2.0);
+
+	cout << "The radius and area of the cirlce are:\n";
+	cout << fixed << setprecision(2);
+	cout << "Radius: " << c.radius << endl;
+	cout << "Area: " << c.area << endl;
+
+	return 0;
+
+
+
 }
+Circle getInfo() {
+	Circle tempCircle;
+	cout << "Enter the diamter of a circle: ";
+	cin >> tempCircle.diamter;
+	tempCircle.radius = tempCircle.diamter / 2.0;
+	
+	return tempCircle;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
