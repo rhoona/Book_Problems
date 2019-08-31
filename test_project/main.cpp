9c4048029b2e17516e5b35c7ba2c1fbbf16eaf54
rhoona <email_id>
2019-08-31 14:31:28 -0700

    just testing on netbeans

---
+++ test_project/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: laptop
+ *
+ * Created on August 31, 2019, 2:29 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    //just testing git on netbeans
+    return 0;
+}
+
